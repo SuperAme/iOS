8ef7baca0aa1776d78a442bfed8efb7c09acddab
AMERICO MENESES QUINTERO <americo.meneses.contractor@bbva.com>
2020-06-09 09:35:27 -0500

    adding shadow to image

--- images/images/ContentView.swift
+++ images/images/ContentView.swift
@@ -11,7 +11,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Image("css").resizable().edgesIgnoringSafeArea(.vertical)
+        Image("fit")
+            .resizable()
+            //.scaledToFit()
+            .aspectRatio(contentMode: .fill)
+            .frame(width:450)
+            //.clipped()
+            .clipShape(Capsule())
+            .opacity(0.6)
+            .overlay(
+//                Image(systemName: "heart.fill")
+//                    .font(.system(size: 60))
+//                .foregroundColor(.red)
+//                .opacity(0.9)
+                
+//                Text("Entrena con nosotros")
+//                    .fontWeight(.bold)
+//                    .font(.system(size:30, design: .rounded))
+//                    .foregroundColor(.black)
+//                    .padding()
+//                    .background(Color.gray)
+//                    .cornerRadius(100)
+//                    .opacity(0.7)
+//                    .padding(),
+//                alignment: .bottom
+                Capsule()
+                    .foregroundColor(.pink)
+                    .opacity(0.5)
+                    .overlay(
+                        Text("Train with us")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 250)
+                )
+            )
+                    
+            //.edgesIgnoringSafeArea(.vertical)
         
 //        Image(systemName: "scissors.badge.ellipsis")
 //            .font(.system(size: 70))

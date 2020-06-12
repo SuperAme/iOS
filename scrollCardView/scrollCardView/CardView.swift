ba8df3ee93aab765f082fac07f1570e9674743d4
AMERICO MENESES QUINTERO <americo.meneses.contractor@bbva.com>
2020-06-12 11:37:24 -0500

    addign different card views

--- scrollCardView/scrollCardView/CardView.swift
+++ scrollCardView/scrollCardView/CardView.swift
@@ -9,37 +9,49 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    var imageName: String
+    var authorName: String
+    var courseTitle: String
+    var originalPrice: String
+    var disccountprice: String
+    
     var body: some View {
         VStack{
-            Image("ios_img")
+            Image(imageName)
             .resizable()
                 .aspectRatio(contentMode: .fit)
-            VStack(alignment: .leading){
-                Text("Américo MQ")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Text("Diseño de app con swift Ui para iOS 13 desde cero".uppercased())
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.primary)
-                    .lineLimit(2)
-                HStack {
-                    Text("$45.50")
-                        .font(.subheadline)
-                        .foregroundColor(.primary)
-                    Text("$99.99")
-                        .font(.subheadline)
+            HStack {
+                VStack(alignment: .leading){
+                    Text(authorName)
+                        .font(.headline)
                         .foregroundColor(.secondary)
-                        .strikethrough()
-                }
-            }
+                    Text(courseTitle.uppercased())
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    HStack {
+                        Text(disccountprice)
+                            .font(.subheadline)
+                            .foregroundColor(.primary)
+                        Text(originalPrice)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .strikethrough()
+                    }
+                }.layoutPriority(10)
+                Spacer()
+            }.padding()
         }
-        
+    .cornerRadius(12)
+        .overlay(RoundedRectangle(cornerRadius: 12).stroke(Color(red: 130/255, green: 130/255, blue: 130/255, opacity: 0.2), lineWidth: 2))
+        .padding([.top, .horizontal])
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(imageName: "ios_img", authorName: "Américo MQ", courseTitle: "Diseño de Apps con Swuift UI para iOS 13 desde cero", originalPrice: "$99.99", disccountprice: "$49.99")
     }
 }
--- scrollCardView/scrollCardView/ContentView.swift
+++ scrollCardView/scrollCardView/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView{
+            VStack{
+                Text("Américo MQ")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                Text("Desarrollador front end")
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+            }
+            CardView(imageName: "ios_img", authorName: "Américo MQ", courseTitle: "Diseño de Apps con Swuift UI para iOS 13 desde cero", originalPrice: "$99.99", disccountprice: "$49.99")
+            CardView(imageName: "ios_img0", authorName: "Américo MQ", courseTitle: "Diseño de Apps con Swuift UI para iOS 13 desde cero", originalPrice: "$99.99", disccountprice: "$49.99")
+            CardView(imageName: "ios_img1", authorName: "Américo MQ", courseTitle: "Diseño de Apps con Swuift UI para iOS 13 desde cero", originalPrice: "$99.99", disccountprice: "$49.99")
+            CardView(imageName: "ios_img2", authorName: "Américo MQ", courseTitle: "Diseño de Apps con Swuift UI para iOS 13 desde cero", originalPrice: "$99.99", disccountprice: "$49.99")
+        }
     }
 }
 

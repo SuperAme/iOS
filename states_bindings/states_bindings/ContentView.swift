b7a181e935135a93e3b04f26466feb8bec0b45c4
AMERICO MENESES QUINTERO <americo.meneses.contractor@bbva.com>
2020-06-15 10:58:05 -0500

    clicker button

--- states_bindings/states_bindings/ContentView.swift
+++ states_bindings/states_bindings/ContentView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isPlaying = false
+    @State private var num:Int = 0
     
     var body: some View {
         Button(action: {
-            self.isPlaying.toggle()
+            self.num += 1
         }){
-            Image(systemName:isPlaying ? "pause.rectangle.fill" : "play.rectangle.fill")
-            .font(.system(size: 100))
-            .foregroundColor(.red)
+            Circle()
+            .frame(width: 90, height: 90)
+            .foregroundColor(Color.blue)
+            .overlay(Text("\(num)")
+                .font(.system(size: 60))
+                .foregroundColor(Color.white))
         }
+        
+        
+       
+        
+//        Button(action: {
+//            self.isPlaying.toggle()
+//        }){
+//            Image(systemName:isPlaying ? "pause.rectangle.fill" : "play.rectangle.fill")
+//            .font(.system(size: 100))
+//            .foregroundColor(.red)
+//        }
         
     }
 }
